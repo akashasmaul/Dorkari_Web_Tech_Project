47a05229c5f1d9d368ef9b4daa7c89f85676be89
akashasmaul <akashasmaulhossain@gmail.com>
2022-12-08 22:26:37 +0600

    added ti github

---
+++ view/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    
+<footer class="w3-container w3-padding-64 w3-center w3-opacity"> 
+  <div class="w3-large w3-padding-16">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">   
+
+<h4>Find Us</h4>
+	<h5>123 Street, Dhaka, bangladesh</h5>
+	<h5>01789123456</h5>
+	<h5>info@chakribakri.com</h5>
+    <a href="../view/home.php">Home</a>
+  <a href="../view/Service.php">Our Services</a>
+  <a href="../view/contact.php">Contact US</a>
+  <a href="../view/PrivacyPolicy.php">Privacy Policy</a>
+  <a href="../view/tnc.php">Terms & Condition</a>
+  <a href="../view/registation.php">SignUp</a>
+  <center><h3 style="color: white;">Copyright © 2022 Dorkari Platform Limited | All Rights Reserved.</h1></center>
+</div>
+ </footer>
+</body>
+</html>
