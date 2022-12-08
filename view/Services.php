b7de6de1c4cc36400c054d0d1f090cc1299e8af7
akashasmaul <akashasmaulhossain@gmail.com>
2022-12-09 01:22:09 +0600

    services

--- view/Services.php
+++ view/Services.php
@@ -10,29 +10,60 @@ include "header.php";
 ?>
 </head>
 <body>
-   
-<a href="../view/homeappliances.php">
-<img src="../images/home2.gif" alt="home" width="400" height="250">
-</a>
 
-<a href="painting.php">
-    <img src="../images/painting.gif" alt="paint" width="400" height="250">
-</a>
+<!-- !PAGE CONTENT! -->
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
 
-<a href="car.php">
-    <img src="../images/car2.gif" alt="car" width="400" height="250">
-</a>
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="homeappliances">
+  
+    <div class="w3-third">
+    <a href="../view/homeappliances.php">
+      <img src="../images/home2.gif" alt="home" style="width:100%" width="400" height="250">
+      <h3>Home Appliances</h3>      
+      </a> <p>Repire: TV, Fridge, Oven, Heater, AC</p> </div>    
 
-<a href="Pest Control.php">
-    <img src="../images/pest.gif" alt="pesting" width="400" height="250">
-</a>
+    <div class="w3-third">
+    <a href="../view/painting.php">
+    <img src="../images/painting.gif" alt="paint" style="width:100%" width="400" height="250">
+    <h3>Painting Services</h3> </a>
+      <p>Find out our all out painting services</p>
+    </div>
 
-<a href="laundry.php">
-    <img src="../images/laundry.gif" alt="laundry" width="400" height="250">
-</a>
+    <div class="w3-third">
+    <a href="../view/car.php">
+      <img src="../images/car2.gif" alt="car" style="width:100%" width="400" height="250">
+      <h3>Car Services</h3> </a>
+      <p>Car Wash, Mechanical Work, PaintJob</p>      
+    </div>    
+  </div>
+  <center><p>What else?</p></center>
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+  
+    <div class="w3-third">
+    <a href="../view/PestControl.php">
+    <img src="../images/pest.gif" alt="pest" style="width:100%" width="400" height="250">
+    <h3>Pest Control</h3> </a> 
+    <p>We offer insects cleaning service</p> </div>
 
-<a href="#driver"></a>
-<img src="../images/driver.gif" alt="driver" width="400" height="250">
+    <div class="w3-third">
+    <a href="../view/laundry.php">
+    <img src="../images/laundry.gif" alt="laundry" style="width:100%" width="400" height="250">
+    <h3>Laundry Service</h3> </a>
+    <p>We offer high quality washing with WASHING POWDER NIRMA.</p>
+    </div>  
+      
+      
+    <div class="w3-third">
+    <a href="../view/404.php">
+      <img src="../images/driver.gif" alt="driver" style="width:100%" width="400" height="250">
+      <h3>Driver Service</h3> </a>
+      <p>You can hire drivers on contract</p>
+    </div>
+
+  </div>
+
 
 <?php
 include "footer.php";                
